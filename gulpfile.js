1e925e44302287edb955168532ab58d28e633be5
Janvi Patel <pateljanvi123.jp@gmail.com>
2020-12-09 19:46:48 -0600

    install some of the gulp packages to the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 const {src, dest, series, parallel} = require('gulp');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 
 //html Task
 function htmlTask() {
@@ -9,6 +14,9 @@ function htmlTask() {
 //css Task
 function stylesTask() {
     return src('src/styles/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
     .pipe(dest('dist/css'))
 }
 
@@ -21,7 +29,12 @@ function scriptsTask() {
 //images Task
 function imagesTask() {
     return src('src/images')
+    .pipe(imagemin())
     .pipe(dest('dist/images'))
 }
 
+exports.html = htmlTask;
+exports.styles = stylesTask;
+exports.scripts = scriptsTask;
+exports.images = imagesTask;
 exports.default = series(htmlTask, stylesTask, scriptsTask, imagesTask);
